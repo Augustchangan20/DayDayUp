2d54cf67160bed2ddc6ff31a03e4aa23bb0d416b
Twelve <157513646@qq.com>
2020-11-27 10:39:01 +0800

    11_24 practice demo

---
+++ RepeatedPractice/Demo_1124.java
@@ -0,0 +1,27 @@
+/**
+ *1、toLowerCase():  把字符串中的大写字母改为小写字母
+ *2、toUpperCase():  把字符串中的小写字母改为大写字母
+ */
+
+class Base{
+    /*Base(){
+        System.out.print("Base");
+    }*/
+}
+public class Demo_1124 extends Base {
+    public static void main(String[] args) {
+        new Demo_1124();
+        new Base();
+
+    }
+
+
+
+   /* public static boolean isAdmin(String userID){
+        return userID.toLowerCase()=="admin";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAdmin("Admin"));
+    }*/
+}
