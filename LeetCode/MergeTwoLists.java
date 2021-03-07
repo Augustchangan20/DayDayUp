e83702cd9a14e9ae2399538468a85f5c8e5aa1a1
Twelve <157513646@qq.com>
2021-03-07 10:56:31 +0800

    合并有序链表

--- LeetCode/MergeTwoLists.java
+++ LeetCode/MergeTwoLists.java
@@ -8,10 +8,10 @@
 }
  class MergeTwoLists {
     public static ListNode mergeTwoLists(ListNode l1,ListNode l2){
-        ListNode newHead = new ListNode(-1);//构建辅助头节点
+        ListNode newHead = new ListNode(-1);//构建辅助头节点,输出的时候输出虚拟结点的下一个结点就OK
         ListNode tmp = newHead;
-        while (l1 != null && l2!= null){
-            if (l1.data < l2.data){
+        while (l1 != null && l2!= null){//终止条件，有一方为空则终止
+            if (l1.data < l2.data){//比较后，小的节点放前面
                 tmp.next = l1;
                 tmp = tmp.next;
                 l1 = l1.next;
@@ -21,12 +21,12 @@
                 l2 = l2.next;
             }
         }
-        if (l1 != null){
+        if (l1 != null){//有一方为空，虚拟节点直接连接非空链表
             tmp.next = l1;
         }
         if (l2 != null){
             tmp.next = l2;
         }
-        return newHead.next;
+        return newHead.next;//返回虚拟节点的下一个节点
     }
 }
